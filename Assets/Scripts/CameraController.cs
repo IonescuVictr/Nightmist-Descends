c1dbb0e450a5791aa60f31c4b9c12f496fb5f7fe
Victor Ionescu <ionescu.victor21@gmail.com>
2017-08-30 19:52:52 +0300

    Initial project revision

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target; //refference to target
+    public float lookSmooth = 0.09f; //how fast we want to look at the target
+    public Vector3 offsetFromTarget = new Vector3(0, 6, -8); //how far we want te camera to be from the player
+    public float xTilt = 10; //xTilt how far we want our camera to be rotated on the X axis. enables us to look downward on our target
+
+    Vector3 destination = Vector3.zero;//where the camera moves to
+    CharacterController characterController;//need a CharacterController refference so that we can acces the target rotation of the caracter
+    float rotateVel = 0;
+
+    private void Start()
+    {
+        SetCameraTarget(target);
+    }
+
+    void SetCameraTarget(Transform t)//to have te ability to give the camera a new target to look at
+    {
+        target = t;
+
+        if (target != null)
+        {
+            if (target.GetComponent<CharacterController>())
+            {
+                characterController = target.GetComponent<CharacterController>();
+            }
+            else
+                Debug.LogError("Your camera needs a target.");
+        }
+        else
+            Debug.LogError("Your camera needs a target.");
+    }
+
+    private void LateUpdate()//Update()-once per frame, FixedUpdate()-multyple times per frame, LateUpdate()-only after the update() calls
+        //It's ensuring that we are doing movements and rotations based off on the most previous positions of the character
+    {
+        //moving
+        MoveToTarget();
+        //rotating
+        LookAtTarget();
+    }
+
+    void MoveToTarget()
+    {
+        destination = characterController.TargetRotation * offsetFromTarget;//offsetfromtarget is how far away we want the camera from the target.
+        destination += target.position; //making sure that destination(rotated point) is relative to our target
+        transform.position = destination;
+    }
+
+    void LookAtTarget()
+    {
+        float eulerYAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, target.eulerAngles.y, ref rotateVel, lookSmooth);
+        //transform.rotation = Quaternion.Euler(transform.eulerAngles.x, eulerYAngle, 0);
+        transform.rotation = Quaternion.Euler(xTilt, eulerYAngle, 0);
+    }
+
+}
---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    public float inputDelay = 0.1f; //if the player instantly presses the input button to move forward there will be a delay
+    public float forwardVelocity = 12;
+    public float rotateVelocity = 100;
+
+    Quaternion targetRotation; // Quaternion objects that hold rotations on a scale from -1 to 1 on each axis. Also has a 4th axis.
+    Rigidbody rBody; //rigidbody refference(need it for unity 5)
+    float forwardInput, turnInput;
+
+    public Quaternion TargetRotation//property for target rotation
+        {
+         get{return targetRotation;}//need to accces from camera controller. to base camera rot of character rot
+        }
+
+    private void Start()// Awake() & Start(). Like the Awake function, Start is called exactly once in the lifetime of the script.However,
+        //Awake is called when the script object is initialised, regardless of whether or not the script is enabled.
+        //Start may not be called on the same frame as Awake if the script is not enabled at initialisation time
+    {
+
+        targetRotation = transform.rotation; //sets targetrot equal to initial rotation, when we first spawn
+
+        if (GetComponent<Rigidbody>())
+            rBody = GetComponent<Rigidbody>();
+        else
+            Debug.LogError("The character needs a rigidboyd.");
+
+        forwardInput = turnInput = 0;
+    }
+
+    void GetInput()//method for getting keyboard inputs
+    {
+        forwardInput = Input.GetAxis("Vertical"); //get axis return a value from -1 to 1
+        turnInput = Input.GetAxis("Horizontal");
+    }
+
+    private void Update()//without private in tutorial. Update is called every frame
+    {
+        GetInput();
+        Turn();
+    }
+
+    private void FixedUpdate()//has the same time between calls. Used for anything that affects a rigid body(recommended)
+    {
+        Run();
+    }
+
+    void Run()
+    {
+        if (Mathf.Abs(forwardInput) > inputDelay)//checks for deadzone. Abs(value)because forward input can be negative
+        {
+            //move
+            rBody.velocity = transform.forward * forwardInput * forwardVelocity; 
+        }
+        else
+            //zero  velocity
+            rBody.velocity = Vector3.zero; //if computer doesn't recognize our imput, we make sure the character isn't moving at all
+    }
+
+    void Turn()
+    {
+        if (Mathf.Abs(turnInput) > inputDelay)//checks for deadzone
+        {
+            targetRotation *= Quaternion.AngleAxis(rotateVelocity * turnInput * Time.deltaTime, Vector3.up);//AngleAxis(what angle to we want to rotate to, on what axis are we making this rotation)
+            //multiplying quaternions is like adding angles
+        }
+        transform.rotation = targetRotation; //attributing the news values to rotation
+
+    }
+
+}
